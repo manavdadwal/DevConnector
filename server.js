db46a49235032bdb791a740fd29e56f7ffb59a27
Manav Dadwal <manav.dadwal@ril.com>
2025-07-16 17:01:26 +0530

    MongoDB Connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const connect = require("./config/db");
 const app = express();
+
+// Connect DB
+connect();
 
 app.get("/", (req, res) => res.send("API Running"));
 
