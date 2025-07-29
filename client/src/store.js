c1dcaf0190fa6b7057dc7e0bcbe57bc71a7315e2
Manav Dadwal <manav.dadwal@ril.com>
2025-07-29 15:56:17 +0530

    Added Redux store which will hold app’s global state , update state (rootReducer),  Support async logic (via redux-thunk) & also connect React components via hooks like useSelector and useDispatch.

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initialState = {};
-
-const middleware = [thunk];
-
-const store = createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware, ...middleware)
-);
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(), // default includes thunk
+    devTools: composeWithDevTools(), // process.env.NODE_ENV !== "production",
+});
 
 export default store;
