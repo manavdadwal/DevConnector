663b940028c7283e686321730b2a060423320e76
Manav Dadwal <manav.dadwal@ril.com>
2025-08-26 16:58:34 +0530

    Added education and exp components

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "components/routing/PrivateRoute";
 import CreateProfile from "components/profile-forms/CreateProfile";
 import EditProfile from "components/profile-forms/EditProfile";
+import AddExperience from "components/profile-forms/AddExperience";
+import AddEducation from "components/profile-forms/AddEducation";
 // REDUX
 import { Provider } from "react-redux";
 import store from "./store";
@@ -55,6 +57,22 @@ const App = () => {
                             element={
                                 <PrivateRoute>
                                     <EditProfile />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route
+                            path="/add-experience"
+                            element={
+                                <PrivateRoute>
+                                    <AddExperience />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route
+                            path="/add-education"
+                            element={
+                                <PrivateRoute>
+                                    <AddEducation />
                                 </PrivateRoute>
                             }
                         />
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_PROFILE, PROFILE_ERROR } from "actions/types";
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "actions/types";
 import { setAlert } from "./alert";
 
 // Get Current Profile
@@ -49,6 +49,94 @@ export const createProfile =
             if (!edit) {
                 navigate("/dashboard");
             }
+        } catch (err) {
+            const errors = err.response.data.errors;
+
+            if (errors) {
+                errors.forEach((error) => {
+                    dispatch(setAlert(error.msg, "danger"));
+                });
+            }
+
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: {
+                    msg: err.response.statusText,
+                    status: err.response.status,
+                },
+            });
+        }
+    };
+
+// Add Experience
+
+export const addExperience =
+    (formData, navigate, history) => async (dispatch) => {
+        try {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+
+            const res = await axios.put(
+                "api/profile/experience",
+                formData,
+                config
+            );
+
+            dispatch({
+                type: UPDATE_PROFILE,
+                payload: res.data,
+            });
+
+            dispatch(setAlert("Experience Added", "success"));
+
+            navigate("/dashboard");
+        } catch (err) {
+            const errors = err.response.data.errors;
+
+            if (errors) {
+                errors.forEach((error) => {
+                    dispatch(setAlert(error.msg, "danger"));
+                });
+            }
+
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: {
+                    msg: err.response.statusText,
+                    status: err.response.status,
+                },
+            });
+        }
+    };
+
+//Add Education
+
+export const addEducation =
+    (formData, navigate, history) => async (dispatch) => {
+        try {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+
+            const res = await axios.put(
+                "api/profile/education",
+                formData,
+                config
+            );
+
+            dispatch({
+                type: UPDATE_PROFILE,
+                payload: res.data,
+            });
+
+            dispatch(setAlert("Education Added", "success"));
+
+            navigate("/dashboard");
         } catch (err) {
             const errors = err.response.data.errors;
 
